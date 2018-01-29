e01f30f588deda2731639cdfdd5b2e747213b43d
Anirudh Ryali <aryali97@gmail.com>
2018-01-29 17:36:35 -0500

    Restructured Pig to reduce lines of code

--- pig.py
+++ pig.py
@@ -5,56 +5,45 @@ import random
 print("Welcome to Pig!")
 
 done = False
-player_temp_total = 0
+temp_total = 0
 player_total = 0
-comp_temp_total = 0
 comp_total = 0
-turn = "player"
-winning_score = 50
+player_turn = True
+winning_score = 10
 
 while not done:
-    while turn == "player" and not done:
-        print()
-        print("Player:", player_total, "Computer:", comp_total)
+    print()
+    print("Player:", player_total, "Computer:", comp_total)
+    if player_turn:
         print("It's your turn!")
-        roll = random.randint(1,6)
-        print("You rolled a", roll)
-        if roll == 1:
-            turn = "computer"
-            player_temp_total = 0
-            print("PIG! Too bad! Your total is currently:", player_total)
-        else:
-            player_temp_total += roll
-            print("You currently have " + str(player_temp_total) + " banked.")
+    else:
+        print("It's the computer's turn!")
+    roll = random.randint(1,6)
+    print("You" if player_turn else "The computer", "rolled a", roll)
+    if roll == 1:
+        player_turn = not player_turn
+        temp_total = 0
+        print("PIG! Too bad! Your total is currently:", player_total)
+    else:
+        temp_total += roll
+        print("You currently have " if player_turn else "The computer has ",str(temp_total) + " banked.")
+        if player_turn:
             choice = input("Do you wish to roll again (y/n)?: ")
             if choice == 'n':
-                player_total += player_temp_total
-                player_temp_total = 0
-                print("Your total socre is now:", player_total)
-                turn = "computer"
-        if player_total > winning_score:
-            print("You win! " + str(player_total) + " to " + str(comp_total))
-            done = True
-
-    while turn == "computer" and not done:
-        print()
-        print("Player:", player_total, "Computer:", comp_total)
-        print("It's the computer's turn!")
-        roll = random.randint(1,6)
-        print("The computer rolled a", roll)
-        if roll == 1:
-            turn = "player"
-            comp_temp_total = 0
-            print("PIG! Too bad! The computer's total is currently:", comp_total)
+                player_total += temp_total
+                temp_total = 0
+                print("Your total score is now:", player_total)
+                player_turn = not player_turn
         else:
-            comp_temp_total += roll
-            print("The computer has " + str(comp_temp_total) + " banked.")
-            if comp_temp_total > 6 or comp_total + comp_temp_total > winning_score:
+            if temp_total > 6 or comp_total + temp_total > winning_score:
                 print("The computer has chosen to end its turn.")
-                comp_total += comp_temp_total
-                comp_temp_total = 0
+                comp_total += temp_total
+                temp_total = 0
                 print("The computer's score is now:", comp_total)
-                turn = "player"
-        if comp_total > winning_score:
-            print("The computer wins! " + str(comp_total) + " to " + str(player_total))
-            done = True
+                player_turn = not player_turn
+    if not player_turn and player_total > winning_score:
+        print("You win! " + str(player_total) + " to " + str(comp_total))
+        done = True
+    if player_turn and comp_total > winning_score:
+        print("The computer wins! " + str(comp_total) + " to " + str(player_total))
+        done = True
