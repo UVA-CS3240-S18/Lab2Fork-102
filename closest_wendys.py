76b09c5a4af490de8e1e754dc232dea209ae322f
Eric McCord-Snook <esm7ky@virginia.edu>
2018-01-29 17:38:51 -0500

    Changed Wendy's distance method to perform more efficiently and use fewer lines of code.

--- closest_wendys.py
+++ closest_wendys.py
@@ -6,36 +6,35 @@ import webbrowser
 google_maps_url = "https://www.google.com/maps/@35.372742,-81.954957,15z?hl=en"
 
 def distance_between(lat_1, lon_1, lat_2, lon_2):
-    theta = lon_1 - lon_2
-    dist = math.sin(lat_1 * math.pi / 180.0) * math.sin(lat_2 * math.pi / 180.0) + math.cos(lat_1 * math.pi / 180.0) * math.cos(lat_2 * math.pi / 180.0) * math.cos(theta * math.pi / 180.0)
-    dist = math.acos(dist)
-    dist = dist * 180.0 / math.pi
-    dist = dist * 60 * 1.1515
-
+    conv = math.pi / 180.0
+    lat1Conv = lat_1 * conv
+    lat2Conv = lat_2 * conv
+    dist = math.sin(lat1Conv) * math.sin(lat2Conv) + math.cos(lat1Conv) * math.cos(lat2Conv) * math.cos((lon_1 - lon_2) * conv)
+    dist = math.acos(dist) * 180.0 * 60 * 1.1515 / math.pi
     return dist
 
-#lat = float(input("Current latitude: "))
-#lon = float(input("Current longitude: "))
+def find_closest_wendys():
+    lat = 38.0322727
+    lon = -78.50997339999999
+    datafile = open("wendys.csv", "r")
 
-lat = 38.0322727
-lon = -78.50997339999999
-datafile = open("wendys.csv", "r")
+    closest_dist = 200
+    closest_wendys = ""
 
-closest_dist = 200
-closest_wendys = ""
+    for line in datafile:
+        entry = line.split(";")
+        dist_to_wendys = distance_between(lat, lon, float(entry[0]), float(entry[1]))
+        if dist_to_wendys < closest_dist:
+            google_maps_url = "https://www.google.com/maps?q=" + str(entry[4]) + "+" + str(entry[5]) + "+" + str(entry[6])
+            print(google_maps_url)
+            closest_dist = dist_to_wendys
+            closest_wendys = entry[2]
 
-for line in datafile:
-    entry = line.split(";")
-    dist_to_wendys = distance_between(lat, lon, float(entry[0]), float(entry[1]))
-    if dist_to_wendys < closest_dist:
-        google_maps_url = "https://www.google.com/maps?q=" + str(entry[4]) + "+" + str(entry[5]) + "+" + str(entry[6])
-        print(google_maps_url)
-        closest_dist = dist_to_wendys
-        closest_wendys = entry[2]
+    datafile.close()
 
-datafile.close()
+    print("The closest Wendy's (", closest_wendys, ") is", closest_dist, "miles away.")
+    google_maps_url = google_maps_url.replace(' ', '+')
+    webbrowser.open(google_maps_url)
 
-print("The closest Wendy's (", closest_wendys, ") is", closest_dist, "miles away.")
-google_maps_url = google_maps_url.replace(' ', '+')
-webbrowser.open(google_maps_url)
-
+if __name__ == '__main__':
+    find_closest_wendys()
