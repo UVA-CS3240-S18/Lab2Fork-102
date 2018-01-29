494075a8839c9bb9544a4083fa5da0491044b2f1
michaelvwu <mvw5mf@virginia.edu>
2018-01-29 14:35:59 -0500

    fixing

--- pig.py
+++ pig.py
@@ -1,4 +1,5 @@
 # Mark Sherriff (mss2x)
+# edited by Michael Wu (mvw5mf)
 
 import random
 
@@ -30,7 +31,7 @@ while not done:
             if choice == 'n':
                 player_total += player_temp_total
                 player_temp_total = 0
-                print("Your total socre is now:", player_total)
+                print("Your total score is now:", player_total)
                 turn = "computer"
         if player_total > winning_score:
             print("You win! " + str(player_total) + " to " + str(comp_total))
