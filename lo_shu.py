d582e0c1a1d04f54c442f06c62fb52d029a5d010
jmekavibul <jm9hp@virginia.edu>
2018-01-29 17:40:37 -0500

    Changed occurance when not a Lo Shu Magic Square
    Changes program so that the user is given the exact row/column/diagonal along with numbers the user inputs in that row/column/diagonal

--- lo_shu.py
+++ lo_shu.py
@@ -14,29 +14,33 @@ for i in range(3):
         count += 1
 # print(square)
 print("You entered:")
-for row in square:
-    print(str(row[0]) + '\t' + str(row[1]) + '\t' + str(row[2]))
+for i in range (3):
+    print(str(square[i][0]) + '\t' + str(square[i][1]) + '\t' + str(square[i][2]))
 
 # check rows
-for row in square:
-    if sum(row) != 15:
+for i in range(3):
+    if square[i][0] + square[i][1] + square[i][2] != 15:
         is_square = False
-        print(str(row) + " fails the test!")
+        print("Row " + str(i) + " fails the test!")
+        print(str(square[i][0]) + '\t' + str(square[i][1]) + '\t' + str(square[i][2]))
 
 # check cols
 for i in range(3):
     if square[0][i] + square[1][i] + square[2][i] != 15:
         is_square = False
         print("Column " + str(i) + " fails the test!")
+        print(str(square[0][i]) + '\t' + str(square[1][i]) + '\t' + str(square[2][i]))
 
 # check diagonals
 if square[0][0] + square[1][1] + square[2][2] != 15:
     is_square = False
     print("Left->Right diagonal fails the test!")
+    print(str(square[0][0]) + '\t' + str(square[1][1]) + '\t' + str(square[2][2]))
 
 if square[0][2] + square[1][1] + square[2][0] != 15:
     is_square = False
     print("Right->Left diagonal fails the test!")
+    print(str(square[0][2]) + '\t' + str(square[1][1]) + '\t' + str(square[2][0]))
 
 if not is_square:
     print("This is not a Lo Shu Magic Square!")
